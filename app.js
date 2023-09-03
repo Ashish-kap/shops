ec4cd417e48b7f9157d3768e459ad5f1a9c6c1aa
Ashish kap <ashishkap157@gmail.com>
2023-09-03 16:57:54 +0530

    first commit

--- app.js
+++ app.js
@@ -41,14 +41,6 @@ app.use(session({
 
 
 
-// Limit requests from same API
-// const limiter = rateLimit({
-//     max: 100,
-//     windowMs: 60 * 60 * 1000,
-//     message: 'Too many requests from this IP, please try again in an hour!'
-//   });
-// app.use('/', limiter);
-
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
